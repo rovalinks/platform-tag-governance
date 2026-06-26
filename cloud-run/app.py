e56404d7fb3d3be160d6fe995e95355e510fc8aa
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-26 16:10:23 +0100

    updated app.py to use the new Cloud Run service name and added a new Dockerfile for deployment.

--- cloud-run/app.py
+++ cloud-run/app.py
@@ -3,11 +3,34 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def health():
     return "Healthy", 200
 
+
 @app.route("/", methods=["POST"])
 def receive_event():
-    print(json.dumps(request.json, indent=2))
+
+    event = request.get_json()
+
+    proto = event.get("protoPayload", {})
+    resource = event.get("resource", {})
+
+    project_id = resource.get("labels", {}).get("project_id")
+    zone = resource.get("labels", {}).get("zone")
+    resource_name = proto.get("resourceName")
+    principal = proto.get("authenticationInfo", {}).get("principalEmail")
+
+    print("=" * 80)
+    print("EVENT SUMMARY")
+    print("=" * 80)
+
+    print(f"Project      : {project_id}")
+    print(f"Zone         : {zone}")
+    print(f"Resource     : {resource_name}")
+    print(f"Created By   : {principal}")
+
+    print("=" * 80)
+
     return "OK", 200
