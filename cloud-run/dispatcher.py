943b160fe0e9b1dffbd35e8c404510425f5cf095
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-26 19:26:10 +0100

    updated  handlers package for storage

--- cloud-run/dispatcher.py
+++ cloud-run/dispatcher.py
@@ -1,4 +1,5 @@
 from handlers.compute_handler import handle_compute_instance
+from handlers.bucket_handler import handle_bucket
 
 
 class Dispatcher:
@@ -17,6 +18,9 @@ class Dispatcher:
         print("=" * 80)
         print(f"Resource Type    : {resource_type}")
 
+        #
+        # Compute Engine
+        #
         if resource_type == "gce_instance":
 
             print("Selected Handler : Compute")
@@ -26,6 +30,21 @@ class Dispatcher:
                 registry,
             )
 
+        #
+        # Cloud Storage
+        #
+        elif resource_type == "gcs_bucket":
+
+            print("Selected Handler : Cloud Storage")
+
+            return handle_bucket(
+                event,
+                registry,
+            )
+
+        #
+        # Unsupported resource
+        #
         print("Selected Handler : None")
         print(f"No handler implemented for '{resource_type}'")
 
---
+++ cloud-run/handlers/bucket_handler.py
@@ -0,0 +1,11 @@
+def handle_bucket(
+    event: dict,
+    registry,
+):
+
+    print("=" * 80)
+    print("CLOUD STORAGE HANDLER")
+    print("=" * 80)
+    print("Bucket handler not implemented yet.")
+
+    return "OK"
