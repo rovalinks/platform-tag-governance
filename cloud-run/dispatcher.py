4c1863e755fa6f825103cdf8f3e727e9617936f7
Rohith <rohith.singirikonda@rovalinks.com>
2026-06-27 23:33:52 +0100

    Scaffold Compute resource handlers

--- cloud-run/dispatcher.py
+++ cloud-run/dispatcher.py
@@ -1,5 +1,10 @@
 from handlers.compute_handler import handle_compute_instance
 from handlers.disk_handler import handle_compute_disk
+from handlers.snapshot_handler import handle_compute_snapshot
+from handlers.image_handler import handle_compute_image
+from handlers.address_handler import handle_compute_address
+from handlers.instance_template_handler import handle_compute_instance_template
+from handlers.machine_image_handler import handle_compute_machine_image
 from handlers.storage_handler import handle_storage_bucket
 
 from utils.constants import (
@@ -36,25 +41,28 @@ class Dispatcher:
         )
 
         if resource_type == COMPUTE_INSTANCE:
-
-            return handle_compute_instance(
-                event,
-                registry,
-            )
+            return handle_compute_instance(event, registry)
 
         elif resource_type == COMPUTE_DISK:
+            return handle_compute_disk(event, registry)
 
-            return handle_compute_disk(
-                event,
-                registry,
-            )
+        elif resource_type == COMPUTE_SNAPSHOT:
+            return handle_compute_snapshot(event, registry)
+
+        elif resource_type == COMPUTE_IMAGE:
+            return handle_compute_image(event, registry)
+
+        elif resource_type == COMPUTE_ADDRESS:
+            return handle_compute_address(event, registry)
+
+        elif resource_type == COMPUTE_INSTANCE_TEMPLATE:
+            return handle_compute_instance_template(event, registry)
+
+        elif resource_type == COMPUTE_MACHINE_IMAGE:
+            return handle_compute_machine_image(event, registry)
 
         elif resource_type == STORAGE_BUCKET:
-
-            return handle_storage_bucket(
-                event,
-                registry,
-            )
+            return handle_storage_bucket(event, registry)
 
         banner("NO HANDLER")
 
--- cloud-run/utils/constants.py
+++ cloud-run/utils/constants.py
@@ -1,9 +1,12 @@
 COMPUTE_INSTANCE = "gce_instance"
+COMPUTE_INSTANCE = "gce_instance"
+COMPUTE_DISK = "gce_disk"
+COMPUTE_SNAPSHOT = "gce_snapshot"
+COMPUTE_IMAGE = "gce_image"
+COMPUTE_ADDRESS = "gce_address"
+COMPUTE_INSTANCE_TEMPLATE = "gce_instance_template"
+COMPUTE_MACHINE_IMAGE = "gce_machine_image"
 
 STORAGE_BUCKET = "gcs_bucket"
-
 BIGQUERY_DATASET = "bigquery_dataset"
-
 PUBSUB_TOPIC = "pubsub_topic"
-
-COMPUTE_DISK = "gce_disk"
